4197ea61db59f9f93798b0ca3bf78f1b7e05d50a
rashidsajahan <rashidsajahan@gmail.com>
2024-10-14 20:53:56 +0530

    json-server data fetching, search filter, aync runner fetch fn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,14 @@ import { RouterModule } from '@angular/router';
   template: `
     <main>
       <header class="brand-name">
+        <a [routerLink]="['/']">
         <img
           class="brand-logo"
           src="/assets/logo.svg"
           alt="logo"
           aria-hidden="true"
         />
+        </a>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,12 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form action="submit">
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
-    <section>
-      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css'
@@ -24,8 +24,22 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocation();
+    // this.housingLocationList = this.housingService.getAllHousingLocation();
+    this.housingService.getAllHousingLocation().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
+
+  filterResults(text: string) {
+    if (!text) this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    )
+  }
+
 }
